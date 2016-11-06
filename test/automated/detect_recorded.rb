00c9aef6d8ecb41604744d5f39133bbfe9a9e38d
Scott Bellware <sbellware@gmail.com>
2016-11-06 09:42:37 -0600

    Time precision set to 5 fractional digits. Sink tests use insignificant time values.

--- test/automated/detect_recorded.rb
+++ test/automated/detect_recorded.rb
@@ -3,7 +3,7 @@ require_relative 'automated_init'
 context "Detect Recorded Telemetry" do
   sink = Telemetry::Controls::Sink.example
 
-  sink.record :something, Time.now, 'some data'
+  sink.record :something, 'some_time', 'some data'
 
   test "With predicate" do
     assert(sink.recorded? { |r| r.signal == :something })
--- test/automated/sink_records_telemetry.rb
+++ test/automated/sink_records_telemetry.rb
@@ -3,8 +3,8 @@ require_relative 'automated_init'
 context "Sink Records Telemetry" do
   sink = Telemetry::Controls::Sink.example
 
-  sink.record :something, Time.now, 'some data'
-  sink.record :something_else, Time.now, 'some other data'
+  sink.record :something, 'some_time', 'some data'
+  sink.record :something_else, 'some_time', 'some other data'
 
   test do
     assert(sink.records.length == 2)
